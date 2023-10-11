3af3d8ae31835b5b4786d0bf4419c771b277d4f5
Kamauleonard <leonardmwangi2012@gmail.com>
2023-10-11 14:29:29 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Solution 1
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+// Solution 2
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// Solution 3
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// Solution 4
+document.body.appendChild(newHeader);
